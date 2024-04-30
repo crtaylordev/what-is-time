63ef670719e158a28bad25a582fb39390b9d0f87
CR Taylor <157721366+crtaylordev@users.noreply.github.com>
2024-04-29 21:19:20 -0400

    Fix variable type error in script-main.js

--- script-main.js
+++ script-main.js
@@ -1,22 +1,24 @@
-const userHours = document.querySelector("#hours");
-const userMinutes = document.querySelector("#minutes");
-const songMinutes = document.querySelector("#song-minutes");
-const songSeconds = document.querySelector("#song-seconds");
-const songTitle = document.querySelector("#song-title");
-const songArtist = document.querySelector("#artist-name");
+
+const userHours = document.getElementById('hours');
+const userMinutes = document.getElementById('minutes');
+const songMinutes = document.getElementById('song-minutes');
+const songSeconds = document.getElementById('song-seconds');
+const songTitle = document.getElementById('song-title');
+const songArtist = document.getElementById('artist-name');
+
 const button = document.querySelector("#magic-button");
 const resultPara = document.querySelector("output-text")
 
 const getUserTotalMinutes = () => {
-    let hours = userHours;
-    let minutes = userMinutes;
-    let totalMinutes = hours * 60 - minutes;
+    let hours = +userHours.value;
+    let minutes = +userMinutes.value;
+    let totalMinutes = (hours * 60) + minutes;
     return totalMinutes;
 }
 
 const getSongTotalMinutes = () => {
-    let minutes = songMinutes;
-    let seconds = songSeconds;
+    let minutes = +songMinutes.value;
+    let seconds = +songSeconds.value;
     let totalMinutes = minutes + (seconds / 60);
     return totalMinutes;
 }
@@ -25,5 +27,10 @@ const divMinutesBySong = () => {
     let totalMin = getUserTotalMinutes();
     let totalSong = getSongTotalMinutes();
     let trackPlays = totalMin / totalSong;
-    return trackPlays;
+    return trackPlays.toFixed(2);
 }
+
+button.addEventListener("click", () => {
+    let resultPlays = divMinutesBySong();
+    resultPara.textContent = `${userHours} hours and ${userMinutes} minutes = ${resultPlays} plays of ${songTitle} by ${songArtist}`;
+});
